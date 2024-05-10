5e226a029b615d23ebe61f1962d8e3457e39219c
AhmedFathy241 <ahmedkarram241@gmail.com>
2024-05-10 23:14:29 +0300

    last_update

--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     # path('logout', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
-    path('logout', views.logout_view, name='logout'),
+    # path('logout', views.logout_view, name='logout'),
     path('index', views.index, name='index'),
     path('emptycart', views.emptycart, name='emptycart'),
     path('fullcart', views.fullcart, name='fullcart'),
